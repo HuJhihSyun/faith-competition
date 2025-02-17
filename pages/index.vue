a781a3443dcaf43f27ad7ae0625f9c79687c16a9
HuJhihSyun <cj654vmp@gmail.com>
2025-02-17 13:30:19 +0800

    新增首頁 meta、元件 TaskOption 使用者資訊調整為 useLocalStorage

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
-  import { ref } from 'vue'
+  import { useLocalStorage } from '@vueuse/core'
   import MaleSvg from '@/assets/images/male.svg?skipsvgo'
   import FemaleSvg from '@/assets/images/female.svg?skipsvgo'
 
-  const gender = ref<Boolean>(false)
+  useSeoMeta({
+    title: '2025 傳道777榮耀神',
+    author: '© 2025 Love and Word Church All rights reserved.',
+    copyright: '© 2025 Love and Word Church All rights reserved.',
+    keywords: '教會,傳道,空提,信仰,榮耀神',
+    description:
+      '秉持著聖經真理的教導，效法主愛人如己的精神，讓不同年齡、背景與特質的人，相聚一起學習成長，致力於讓 神所打造的天國理想世界，能夠實現在這地上。',
+    ogTitle: '2025 傳道777榮耀神',
+    ogDescription:
+      '秉持著聖經真理的教導，效法主愛人如己的精神，讓不同年齡、背景與特質的人，相聚一起學習成長，致力於讓 神所打造的天國理想世界，能夠實現在這地上。',
+    ogUrl: 'https://www.loveandword.tw/',
+    ogImage: 'https://www.loveandword.tw/resource/images/img.jpg'
+  })
 
   type UserInformation = {
     name: string
@@ -11,7 +23,7 @@
     gender: boolean
   }
 
-  const userInformation = reactive<UserInformation>({
+  const userInformation = useLocalStorage<UserInformation>('loveWordsEventUserInfo', {
     name: '',
     department: 0,
     gender: false
@@ -25,15 +37,15 @@
   const currentStep = ref<number>(0)
 
   const joinEvent = () => {
-    if (!userInformation.name || !userInformation.department) {
-      if (!userInformation.name) {
+    if (!userInformation.value.name || !userInformation.value.department) {
+      if (!userInformation.value.name) {
         isNameFalse.value = true
 
         setTimeout(() => {
           isNameFalse.value = false
         }, 500)
       }
-      if (!userInformation.department) {
+      if (!userInformation.value.department) {
         isDepartmentFalse.value = true
 
         setTimeout(() => {
@@ -43,6 +55,7 @@
       return
     }
 
+    console.log('userInformation', userInformation)
     currentStep.value += 1
   }
 </script>
@@ -66,15 +79,18 @@
         </ToggleSwitch>
       </div>
       <div class="flex justify-center items-center mt-8">
-        <Button :fn="joinEvent" :shadow="true">參加積分</Button>
+        <Button :fn="joinEvent">參加積分</Button>
       </div>
     </template>
     <template v-else>
-      <h3
-        class="text-white text-2xl sm:text-3xl md:text-4xl text-center mb-10 wen-kai-mono py-6 px-10 whitespace-nowrap"
-      >
+      <h3 class="text-white text-xl sm:text-2xl md:text-3xl text-center mb-4 wen-kai-mono whitespace-nowrap">
         做了什麼？
       </h3>
+      <div class="flex flex-col items-center space-y-4 max-w-[400px] mx-auto">
+        <TaskOption>
+          <span>參加活動</span>
+        </TaskOption>
+      </div>
     </template>
   </div>
 </template>
